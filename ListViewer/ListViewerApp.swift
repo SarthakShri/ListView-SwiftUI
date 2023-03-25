05acb3f0a7d394675862d7be39e72d8fad217ffc
Sarthak Shrivastava <sarthakrishu@gmail.com>
2023-03-25 12:16:52 +0530

    Initial Commit

---
+++ ListViewer/ListViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewerApp.swift
+//  ListViewer
+//
+//  Created by Sarthak Shrivastava on 25/03/23.
+//
+
+import SwiftUI
+
+@main
+struct ListViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
